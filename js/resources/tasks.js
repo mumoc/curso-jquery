a2cb5ec24ef594435bcebf12b4211cd7a8a099d7
Mumo Carlos <mumo.crls@gmail.com>
2013-10-12 00:53:19 -0500

    basic style

--- js/resources/tasks.js
+++ js/resources/tasks.js
@@ -23,15 +23,23 @@ Course.Resources.Tasks = (function(){
   Tasks.prototype.create = function(data){
     data = data || { task: { title: '123', content: '456' } };
     request = this.Handler.create(data);
+    request.done(function(task){
+      _this.renderOne(task, 1, []);
+    });
   };
 
   Tasks.prototype.update = function(data){
     data = data || { task: { id: '1', title: 'Do Laundry', content: 'For Gods Sake' } };
     request = this.Handler.update(data);
+    request.done(function(task){
+      var newLi = $('<li>', { id: task.id }).html(task.title);
+      $('li#' + data.id).replaceWith(newLi);
+    });
   };
 
   Tasks.prototype.destroy = function(id){
     request = this.Handler.destroy(id);
+    request.done(function() { $('li#' + id).remove() });
   };
 
   Tasks.prototype.renderAll = function(tasks){
@@ -40,8 +48,9 @@ Course.Resources.Tasks = (function(){
   };
 
   Tasks.prototype.renderOne = function(task, index, tasks){
-    var newLi = $('<li>', { id: task.id }).html(task.title);
-    _this.taskList.append(newLi);
+    var newLi = $('<li>', { id: task.id, class: 'hidden' }).html(task.title);
+    _this.taskList.append(newLi)
+    newLi.fadeIn();
   };
 
   Tasks.prototype.renderError = function(jqXHR){
